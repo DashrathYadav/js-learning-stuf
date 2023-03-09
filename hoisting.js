002345f2fb9fbec486249ce910a7f10098bc967e
Dashrath <sandip9028162859@gmail.com>
2023-03-09 07:12:07 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,57 @@
+'use strict'
+/* var hoisting
+
+console.log(a)
+var a=5;
+console.log(a)
+
+
+------------------------------------------------------------
+
+
+ function hoisting
+
+
+console.log("before decleration calling sleep()")
+sleep()
+
+function sleep()
+{
+    console.log("I am sleeping");
+    
+}
+console.log("After declearation calling sleep()")
+sleep()
+------------------------------------------------------
+
+
+let & const 
+
+console.log(apple)
+const apple='apple'
+//error cannot access before declaration
+
+--------------------------------------------------
+
+
+apple();
+let apple=function(){
+    console.log("This is apple function")
+}
+Mango();
+let Mango=function(){
+    console.log("This is Mango function")
+}
+
+//Error can not access  before deceleatation
+
+--------------------------------------------------------------------
+
+Mango();
+var Mango=function(){
+    console.log("This is Mango function")
+}
+
+//undefined 
+
+*/
